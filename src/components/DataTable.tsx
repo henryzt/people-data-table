b13ff20b3fc161a0fbb90c54ce7f763fbb10e368
Henry Zhang <henryzhang9802@gmail.com>
2024-03-17 16:57:46 +0000

    feat: add sortable header

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,6 +1,6 @@
 import "./DataTable.css";
-import { Fragment, useState } from "react";
-import { ChevronDown, ChevronRight } from "lucide-react";
+import { Fragment, useEffect, useState } from "react";
+import { ChevronDown, ChevronLeft, ChevronRight } from "lucide-react";
 import { PersonRecord } from "../types";
 import DataTableDetailRow from "./DataTableDetailRow";
 
@@ -8,16 +8,63 @@ interface DataTableProps {
   data: PersonRecord[];
 }
 
+const textSorter = (a: string, b: string) => a.localeCompare(b);
+const numberSorter = (a: number, b: number) => a - b;
+const dateSorter = (a: string, b: string) => new Date(a).getTime() - new Date(b).getTime();
+const booleanSorter = (a: boolean, b: boolean) => (a === b ? 0 : a ? 1 : -1);
+type Sorter = typeof textSorter | typeof numberSorter | typeof dateSorter | typeof booleanSorter;
+
 const DataTable = ({ data }: DataTableProps) => {
   const [currentPage, setCurrentPage] = useState(0);
   const [rowsPerPage, setRowPerPage] = useState(10);
+  const [selectedRow, setSelectedRow] = useState<string>();
+
+  useEffect(() => {
+    // scroll to top and clear selected row
+    window.scrollTo(0, 0);
+    setSelectedRow(undefined);
+  }, [currentPage]);
+
+  const columns: {
+    key: keyof PersonRecord;
+    label: string;
+    sorter?: Sorter;
+  }[] = [
+    { key: "name", label: "Name", sorter: textSorter },
+    { key: "dob", label: "DOB", sorter: dateSorter },
+    { key: "email", label: "Email", sorter: textSorter },
+    { key: "verified", label: "Verified", sorter: booleanSorter },
+    { key: "salary", label: "Salary", sorter: numberSorter },
+  ];
+
+  const [sortKey, setSortKey] = useState<keyof PersonRecord | undefined>();
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
+
+  const handleSort = (key: keyof PersonRecord) => {
+    if (sortKey === key) {
+      setSortDirection((sortDirection) => (sortDirection === "asc" ? "desc" : "asc"));
+    } else {
+      setSortKey(key);
+      setSortDirection("asc");
+    }
+  };
+
+  const sortedData = [...data].sort((a: PersonRecord, b: PersonRecord) => {
+    if (sortKey) {
+      const sorter = columns.find((column) => column.key === sortKey)?.sorter;
+      if (sorter) {
+        return (
+          sorter(a[sortKey] as never, b[sortKey] as never) * (sortDirection === "asc" ? 1 : -1)
+        );
+      }
+    }
+    return 0;
+  });
 
   const startRow = currentPage * rowsPerPage;
   const endRow = startRow + rowsPerPage;
-  const displayedData = data.slice(startRow, endRow);
-  const totalPages = Math.ceil(data.length / rowsPerPage);
-
-  const [selectedRow, setSelectedRow] = useState<string>();
+  const totalPages = Math.ceil(sortedData.length / rowsPerPage);
+  const displayedData = sortedData.slice(startRow, endRow);
 
   return (
     <div className="max-w-4xl mx-auto">
@@ -27,11 +74,17 @@ const DataTable = ({ data }: DataTableProps) => {
           <thead className="border-b">
             <tr>
               <th></th>
-              <th>Name</th>
-              <th>DOB</th>
-              <th>Email</th>
-              <th>Verified</th>
-              <th>Salary</th>
+              {columns.map((column, index) => (
+                <th
+                  key={index}
+                  className="sortable-header"
+                  onClick={() => column.sorter && handleSort(column.key)}
+                >
+                  {column.label}{" "}
+                  {!!column.sorter &&
+                    (sortKey === column.key ? (sortDirection === "asc" ? "↑" : "↓") : "")}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody>
@@ -46,11 +99,9 @@ const DataTable = ({ data }: DataTableProps) => {
                     <td className="w-5 !pr-0 opacity-40">
                       {isSelected ? <ChevronDown size={20} /> : <ChevronRight size={20} />}
                     </td>
-                    <td>{person.name}</td>
-                    <td>{person.dob}</td>
-                    <td>{person.email}</td>
-                    <td>{person.verified ? "Yes" : "No"}</td>
-                    <td>{person.salary}</td>
+                    {columns.map((column, index) => (
+                      <td key={index}>{person[column.key] as string}</td>
+                    ))}
                   </tr>
                   {isSelected && <DataTableDetailRow person={person} />}
                 </Fragment>
@@ -84,13 +135,13 @@ const DataTable = ({ data }: DataTableProps) => {
             onClick={() => setCurrentPage((currentPage) => currentPage - 1)}
             disabled={currentPage === 0}
           >
-            ←
+            <ChevronLeft size={20} />
           </button>
           <button
             onClick={() => setCurrentPage((currentPage) => currentPage + 1)}
             disabled={endRow >= data.length}
           >
-            →
+            <ChevronRight size={20} />
           </button>
         </div>
       </div>
